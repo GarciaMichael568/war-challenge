b74357797f540dd63a8bc1273c71e0bcc2b1d1c3
Michael Garcia <garciamichael.developer@gmail.com>
2022-05-04 21:19:32 -0600

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Michael Garcia on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Michael Garcia on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
